6148410a3728691f7646ce2306b2c53890775b7d
14sourabh7 <sourabhsingh@cedcoss.com>
2022-02-07 13:22:15 +0530

    added one more function in forms to make it more clear less lines of code

--- src/form.js
+++ src/form.js
@@ -1,71 +1,65 @@
 
 // function validating input for comparison
 function checkResult() {
-  var age = document.getElementById("age").value;
-  var weight = document.getElementById("weight").value;
-  var name = document.getElementById("name").value;
-  if (age && weight && name) {
-    if (age >= 5 && age < 21) {
-      display();
+    var age = document.getElementById("age").value;
+    var weight = document.getElementById("weight").value;
+    var name = document.getElementById("name").value;
+    if (age && weight && name) {
+        if (age >= 5 && age < 21) {
+            display();
+        } else {
+            document.getElementById("result").innerHTML =
+                "Please enter age in the range 5-20";
+        }
     } else {
-      document.getElementById("result").innerHTML =
-        "Please enter age in the range 5-20";
+        document.getElementById("result").innerHTML =
+            "Please fill all the fields";
     }
-  } else {
-    document.getElementById("result").innerHTML =
-      "Please fill all the fields";
-  }
 }
 
 // function to display result
 function display() {
-  var age = document.getElementById("age").value;
-  var weight = document.getElementById("weight").value;
-  var name = document.getElementById("name").value;
-  var res = validateForm();
-  if (res !== "perfect") {
-    res += " at the age of " + age;
-  }
-  document.getElementById("result").innerHTML =
-    "Hello " + name + " !!! Your weight is " + res;
+    var age = document.getElementById("age").value;
+
+    var name = document.getElementById("name").value;
+    var res = validateForm();
+    if (res !== "perfect") {
+        res += " at the age of " + age;
+    }
+    document.getElementById("result").innerHTML =
+        "Hello " + name + " !!! Your weight is " + res;
 }
 
 // function to compare age
 function validateForm() {
-  var age = Number(document.getElementById("age").value);
-  var weight = Number(document.getElementById("weight").value);
-  var name = document.getElementById("name").value;
-  if (age <= 7) {
-    if (weight < 15) {
-      return "less than recommended value of 15";
-    } else if (weight <= 20) {
-      return "perfect";
+    var age = Number(document.getElementById("age").value);
+    var weight = Number(document.getElementById("weight").value);
+
+    if (age <= 7) {
+
+        return weightCheck(weight, 15, 20)
+
+    } else if (age <= 10) {
+        return weightCheck(weight, 21, 25)
+
+    } else if (age <= 15) {
+        return weightCheck(weight, 26, 30)
+
     } else {
-      return "greater than recommended value of 20";
+        return weightCheck(weight, 31, 40)
+
     }
-  } else if (age <= 10) {
-    if (weight < 21) {
-      return "less than recommended value of 21";
-    } else if (weight <= 25) {
-      return "perfect";
-    } else {
-      return "greater than recommended value of 25";
+    function weightCheck(weight, minWeight, maxWeight) {
+
+        if (weight < minWeight) {
+            return `less than recommended value of ${minWeight}`
+        }
+        else if (weight > maxWeight) {
+            return `greater than recommended value of ${maxWeight}`
+        }
+        else {
+            return 'perfect'
+        }
     }
-  } else if (age <= 15) {
-    if (weight < 26) {
-      return "less than recommended value of 26";
-    } else if (weight <= 30) {
-      return "perfect";
-    } else {
-      return "greater than recommended value of 30";
-    }
-  } else {
-    if (weight < 31) {
-      return "less than recommended value of 31";
-    } else if (weight <= 40) {
-      return "perfect";
-    } else {
-      return "greater than recommended value of 40";
-    }
-  }
+
 }
