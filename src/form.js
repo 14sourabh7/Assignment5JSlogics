1fbb0bcbcc412d091a2b71e8d7b5eda1c6437393
14sourabh7 <sourabhsingh@cedcoss.com>
2022-02-07 13:35:10 +0530

    differentiated css files

--- src/form.js
+++ src/form.js
@@ -1,4 +1,3 @@
-
 // function validating input for comparison
 function checkResult() {
     var age = document.getElementById("age").value;
@@ -6,23 +5,19 @@ function checkResult() {
     var name = document.getElementById("name").value;
     if (age && weight && name) {
         if (age >= 5 && age < 21) {
-            display();
+            display(age, weight, name);
         } else {
             document.getElementById("result").innerHTML =
                 "Please enter age in the range 5-20";
         }
     } else {
-        document.getElementById("result").innerHTML =
-            "Please fill all the fields";
+        document.getElementById("result").innerHTML = "Please fill all the fields";
     }
 }
 
 // function to display result
-function display() {
-    var age = document.getElementById("age").value;
-
-    var name = document.getElementById("name").value;
-    var res = validateForm();
+function display(age, weight, name) {
+    var res = validateForm(age, weight);
     if (res !== "perfect") {
         res += " at the age of " + age;
     }
@@ -31,37 +26,25 @@ function display() {
 }
 
 // function to compare age
-function validateForm() {
-    var age = Number(document.getElementById("age").value);
-    var weight = Number(document.getElementById("weight").value);
-
+function validateForm(age, weight) {
     if (age <= 7) {
-
-        return weightCheck(weight, 15, 20)
-
+        return weightCheck(weight, 15, 20);
     } else if (age <= 10) {
-        return weightCheck(weight, 21, 25)
-
+        return weightCheck(weight, 21, 25);
     } else if (age <= 15) {
-        return weightCheck(weight, 26, 30)
-
+        return weightCheck(weight, 26, 30);
     } else {
-        return weightCheck(weight, 31, 40)
-
+        return weightCheck(weight, 31, 40);
     }
 
-    // function to check weight 
+    // function to check weight
     function weightCheck(weight, minWeight, maxWeight) {
-
         if (weight < minWeight) {
-            return `less than recommended value of ${minWeight}`
-        }
-        else if (weight > maxWeight) {
-            return `greater than recommended value of ${maxWeight}`
-        }
-        else {
-            return 'perfect'
+            return `less than recommended value of ${minWeight}`;
+        } else if (weight > maxWeight) {
+            return `greater than recommended value of ${maxWeight}`;
+        } else {
+            return "perfect";
         }
     }
-
 }
